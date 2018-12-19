c4439a6f289cc683f1ecaa81ea11475958b8b870
Moritz Raguschat <moritz@jumplink.me>
2018-12-19 16:55:20 +0100

    [Order/ClientDetails interface] fix types and property name

--- models/client_details.ts
+++ models/client_details.ts
@@ -4,30 +4,30 @@ export interface ClientDetails extends ShopifyObject {
     /** 
      * Shopify does not offer documentation for this field.
      */
-    accept_language?: string;
+    accept_language: string | null;
 
     /** 
      *  The browser screen height in pixels, if available.
      */
-    browser_height?: string;
+    browser_height: number | null;
 
     /** 
      *  The browser IP address.
      */
-    browser_ip?: string;
+    browser_ip: string;
 
     /** 
      *  The browser screen width in pixels, if available.
      */
-    browser_width?: string;
+    browser_width: number | null;
 
     /** 
      *  A hash of the session.
      */
-    session_height?: string;
+    session_hash: string | null;
 
     /** 
      *  The browser's user agent string.
      */
-    user_agent?: string;
+    user_agent: string;
 }
