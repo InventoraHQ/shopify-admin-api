35b385795d927395f3ac2233551350cbd72191be
art-and-code-studio <pascal@artandcode.studio>
2020-09-25 09:55:06 +0200

    Added missing order webhook response interfaces

--- webhooks/interfaces/index.ts
+++ webhooks/interfaces/index.ts
@@ -1,4 +1,7 @@
+export { WebhookDraftOrderDelete } from './draft-order-delete';
+export { WebhookDraftOrderCreate, WebhookDraftOrderUpdate } from './draft-order';
 export { WebhookFulfillmentCreate, WebhookFulfillmentUpdate } from './fulfillment';
+export { WebhookOrdersDelete} from './order-delete';
 export { WebhookOrdersCancelled, WebhookOrdersCreate, WebhookOrdersFulfilled, WebhookOrdersPaid, WebhookOrdersPartiallyFulfilled, WebhookOrdersUpdated } from './order';
 export { WebhookOrderTransactionCreate } from './order-transactions-create';
 export { WebhookOrdersEdited } from './order-edit';
